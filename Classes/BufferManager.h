d1ce9077449beed107830edb3ddfbe4780c0ffb5
Yanira1612 <83191641+Yanira1612@users.noreply.github.com>
2024-06-13 08:02:12 -0500

    Update BufferManager.h

--- Classes/BufferManager.h
+++ Classes/BufferManager.h
@@ -1,59 +1,91 @@
-#ifndef BUFFERPOOLMANAGER_H 
-#define BUFFERPOOLMANAGER_H
+#ifndef BUFFERMANAGER_H
+#define BUFFERMANAGER_H
+
+// Clase BufferPool
+#include "Frame.h"
+#include "Page.h"
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <ctime>
+
 #include "BufferPool.h"
-// Clase BufferManager: Maneja las operaciones del Buffer Pool, 
-// controla solicitudes de página y liberacion de paginas
+
 class BufferManager {
 private:
-    BufferPool* buffer_pool; //Puntero al BufferPool gestionado por el BufferManager
+
+    static const int LRU = 1;
+    static const int MRU = 2;
+    static const int CLOCK = 3;
+
+
+    BufferPool* bufferManager;
+
+    // Obtiene un frame libre cuando el frame no tiene una página cargada en el puntero page
+    Frame* getFreeFrame() ;
+
+    // evictPage() elige una página para reemplazarla en el buffer pool
+    // y la elimina de la tabla de páginas sin no antes escribirla en disco si es necesario
+    Frame* evictPage(int policy) ;
+
+    // chooseVictimFrame() elige la página a reemplazar en el buffer pool
+    Frame* chooseVictimFrame(int policy) ;
+
+    // actualiza la página en disco con la nueva información
+   
+
+    // obtiene la hora actual en formato std::time_t para last_used
+    std::time_t getCurrentTime() ;
 
 public:
-    BufferManager(int buffer_pool_size) { //Constructor para inicializar el BufferManager con un tamaño específico para el BufferPool.
-        buffer_pool = new BufferPool(buffer_pool_size);//bufferPoolSize Tamaño del BufferPool.
-    }
+    // Constructor de BufferPool
+  //  BufferPool(int buffer_pool_size) ;
 
-    ~BufferManager() {//Destructor para liberar la memoria ocupada por el BufferPool
-        delete buffer_pool;
-    }
+    BufferManager(int size);
 
-    /**
-    Solicita una página del BufferPool y realiza pinPage.
-    Parametro: Identificador del bloque a solicitar.
-    Retorna: Puntero al Frame que contiene la página solicitada.
-    */
-    Frame* requestPage(int block_id,int policy) {
-        return buffer_pool->pinPage(block_id,policy);
-    }
+    // Destructor de BufferPool
+    ~BufferManager() ;
 
-    /**
-    Libera una página del BufferPool, determinando si está sucia o no.
-    Parametros: pageId Identificador de la página a liberar,dirty Indica si la página está sucia
-    */
-    void releasePage(int page_id, bool dirty = false) {
-        buffer_pool->unpinPage(page_id, dirty);
-    }
+    void writePageToDisk(Page* page) ;
 
-    // Obtiene una página del BufferPool por su ID.
-    Page* getPage(int page_id) {
-        return buffer_pool->getPage(page_id);
-    }
+    // pinPage() busca una página y la fija, si no está en memoria la carga
+    Frame* pinPage(int block_id,int policy) ;
 
+    // realiza la consulta
+    Frame* requestPage(int block_id,int policy) ;
+
+    // remueve el Pin de la página y se puede marcar como sucia si dirty = true
+    void unpinPage(int page_id, int policy, bool dirty = false) ;
+
+    void releasePage(int page_id, int policy) ;
+
+    bool pinned(int page_id) ;
+
+    bool unpinned(int page_id) ;
+
+    // carga una página en el buffer pool
+    // con un page_id y un bloque de datos
+    Frame* loadPage(int block_id,int policy) ;
+
+    // obtiene la página con el page_id
+    Page* getPage(int page_id);
+
+    Frame* getFrame(int page_id);
+
+    // imprime cada frame y el id de la página que contiene
+    void showFrames(int policy) ;
+
+    // guardar la página
     void savePage(int page_id) {
         Page* pageNew = getPage(page_id);
-        if(pageNew->dirty==true){
-            buffer_pool->writePageToDisk(pageNew);
+        if(getFrame(page_id)->dirty==true){
+            writePageToDisk(pageNew);
         }
         else{
             std::cout << "La pagina no ha sido modificada" << std::endl;
         }
-        
+
     }
 
-
-    //Muestra la información de todos los frames en el BufferPool.
-    void showFrames(int policy) { 
-        buffer_pool->showFrames(policy);
-    }
 };
-
-#endif //BUFFERPOOLMANAGER_H
+#endif //BUFFERMANAGER_H
